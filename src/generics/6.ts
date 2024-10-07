eae9e249f7bd663a0579c345aa5bf12e4b50f55a
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2024-10-07 11:51:28 +0300

    .

--- src/generics/6.ts
+++ src/generics/6.ts
@@ -15,5 +15,5 @@ type Errors = {
    
    // Реалізуйте Params так, 
    // щоб унеможливити поле 'errors' з типу Form
-   type Params = Omit<Form,'errors'>;
+   type Params = Omit<Form,'errors'>; 
    
